faa5159d2d197905200659ce924d7e7df17848d8
cse0518 <g787@naver.com>
2023-03-31 16:46:02 +0900

    feat: job flow 기본 설정

--- src/main/java/com/example/springbatch/job/JobConfig.java
+++ src/main/java/com/example/springbatch/job/JobConfig.java
@@ -16,17 +16,49 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class JobConfig {
 
     private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
 
     @Bean
-    public Job simpleJob(Step step) {
+    public Job simpleJob() {
         return new JobBuilder("simpleJob", jobRepository)
-                .start(step)
+                .start(step1())         // step1 실행
+                    .on("FAILED")// contribution.exitStatus가 FAILED 일 경우
+                    .to(step3())        // step3으로
+                    .on("*")     // step3의 결과와 관계 없이
+                    .end()              // Flow 종료
+                .from(step1())      // step1로부터
+                    .on("*") // contribution.exitStatus가 FAILED 이외의 모든 경우
+                    .to(step2())    // step2로
+                    .next(step3())  // step2 종료 후 step3
+                    .on("*") // step3의 결과와 관계 없이
+                    .end()          // Flow 종료
+                .end() // Job 종료
                 .build();
     }
 
     @Bean
-    public Step simpleStep(PlatformTransactionManager transactionManager) {
-        return new StepBuilder("simpleStep", jobRepository)
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+//              .tasklet((contribution, chunkContext) -> { // 실패인 경우
+//                  contribution.setExitStatus(ExitStatus.FAILED);
+//                  return RepeatStatus.FINISHED;
+//              })
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return new StepBuilder("step3", jobRepository)
                 .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
                 .transactionManager(transactionManager)
                 .build();
